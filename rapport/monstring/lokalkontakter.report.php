b40d190e439f2dff5794b003f5ada4a2106869d0
Marius Mandal <mariusmandal@gmail.com>
2013-10-09 12:08:03 +0200

    wtf?

--- rapport/monstring/lokalkontakter.report.php
+++ rapport/monstring/lokalkontakter.report.php
@@ -75,8 +75,8 @@ class valgt_rapport extends rapport {
 			$brukere = $this->_brukere();
 
 		global $PHPWord;		
-		return $this->woWrite();
 		$section = $this->word_init('landscape');
+		return $this->woWrite();
 
 		$tab = $section->addTable(array('align'=>'center'));
 		$tab->addRow();
