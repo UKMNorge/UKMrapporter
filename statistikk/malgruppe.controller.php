1c70d6f244e113db98a6a9e966e12e171a1b4c32
Marius Mandal <mariusmandal@gmail.com>
2014-01-31 16:11:44 +0100

    fjernet målgruppe for Oslo

--- statistikk/malgruppe.controller.php
+++ statistikk/malgruppe.controller.php
@@ -1,194 +1,196 @@
 <?php
-if($TWIG['stat_type']=='kommune') {
-	foreach($TWIG['kommuner'] as $kommune_name => $kommune_id){
-		$malgruppe = array();
-	
-		// DENNE MØNSTRINGEN
-		$lokalQRY = new SQL("SELECT `season`,`prosent`
-						   FROM `ukm_statistics_malgruppe`
-						   WHERE `k_id` = '#kommune'",
-						   array('kommune' => $kommune_id)
-						  );
-		$lokalRES = $lokalQRY->run();
-		while( $r = mysql_fetch_assoc( $lokalRES ) ) {
-			$malgruppe[ $r['season'] ][ $kommune_name ] = $r['prosent'];
-		}
+if($TWIG['monstring']->fylke->id != 3) {
+	if($TWIG['stat_type']=='kommune') {
+		foreach($TWIG['kommuner'] as $kommune_name => $kommune_id){
+			$malgruppe = array();
 		
-		
-		// SNITT SMÅ MØNSTRINGER
-		$litenQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   WHERE `size` = 'liten'
-						   GROUP BY `season`"
-						  );
-		$litenRES = $litenQRY->run();
-		while( $r = mysql_fetch_assoc( $litenRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt små kommuner' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-		
-		
-		// SNITT STORE MØNSTRINGER
-		$storQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   WHERE `size` = 'stor'
-						   GROUP BY `season`"
-						  );
-		$storRES = $storQRY->run();
-		while( $r = mysql_fetch_assoc( $storRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt store kommuner' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-		
-		
-		// SNITT I FYLKET
-		$fylkeQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   WHERE `f_id` = '#fylke'
-						   GROUP BY `season`",
-						   array('fylke' => $TWIG['monstring']->fylke->id)
-						  );
-		$fylkeRES = $fylkeQRY->run();
-		while( $r = mysql_fetch_assoc( $fylkeRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt '. $TWIG['monstring']->fylke->name ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-		
-		// SNITT NASJONALT
-		$nasjonalQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   GROUP BY `season`"
-						  );
-		$nasjonalRES = $nasjonalQRY->run();
-		while( $r = mysql_fetch_assoc( $nasjonalRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt nasjonalt' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-		
-		// FJERN 2009
-		unset( $malgruppe[2009] );
-		// SORTER
-		ksort( $malgruppe );
-		// "LAGRE"
-		$TWIG['statistikk'][$kommune_id]['malgruppe'] = $malgruppe;
-	}
-} elseif( $TWIG['stat_type'] == 'fylke' ) {
-		// SNITT I FYLKET
-		$fylkeQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   WHERE `f_id` = '#fylke'
-						   GROUP BY `season`",
-						   array('fylke' => $TWIG['monstring']->fylke->id)
-						  );
-		$fylkeRES = $fylkeQRY->run();
-		while( $r = mysql_fetch_assoc( $fylkeRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt '. $TWIG['monstring']->fylke->name ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-		
-		// SNITT NASJONALT
-		$nasjonalQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
-						   FROM `ukm_statistics_malgruppe`
-						   GROUP BY `season`"
-						  );
-		$nasjonalRES = $nasjonalQRY->run();
-		while( $r = mysql_fetch_assoc( $nasjonalRES ) ) {
-			$malgruppe[ $r['season'] ][ 'Snitt nasjonalt' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
-		}
-
-		// BESTE OG DÅRLIGSTE KOMMUNE
-		$topbotQry = new SQL("SELECT `name`,
-									 `ukm_statistics_malgruppe`.`k_id`,
-									 `season`,
-									 SUM(`malgruppe`) AS `malgruppe`, 
-									 SUM(`deltakere`) AS `deltakere`
+			// DENNE MØNSTRINGEN
+			$lokalQRY = new SQL("SELECT `season`,`prosent`
 							   FROM `ukm_statistics_malgruppe`
-							   LEFT JOIN `smartukm_kommune` AS `k` ON (`k`.`id` = `ukm_statistics_malgruppe`.`k_id`)
-							   WHERE `f_id` = '#fylke'
-							   GROUP BY `season`, `k_id`
-							   ORDER BY `k_id`, `season`",
-							 array('fylke' => $TWIG['monstring']->fylke->id)
-							);
-		$topbotRes = $topbotQry->run();
-		while( $r = mysql_fetch_assoc( $topbotRes ) ) {
-			$data = array( 'malgruppe' => $r['malgruppe'], 'deltakere' => $r['deltakere'], 'dekningsgrad' => round((100/$r['malgruppe'])*$r['deltakere'],2) );
-			$kommune[ $r['k_id'] ][ $r['season'] ] = $data;
-			if($r['season'] != 2009) {
-				$TWIG['dekningsgrad'][ utf8_encode($r['name'])][ $r['season'] ] = $data;
+							   WHERE `k_id` = '#kommune'",
+							   array('kommune' => $kommune_id)
+							  );
+			$lokalRES = $lokalQRY->run();
+			while( $r = mysql_fetch_assoc( $lokalRES ) ) {
+				$malgruppe[ $r['season'] ][ $kommune_name ] = $r['prosent'];
 			}
-			$kommune[ $r['k_id'] ][ 'total' ]['malgruppe'] += $r['malgruppe'];
-			$kommune[ $r['k_id'] ][ 'total' ]['deltakere'] += $r['deltakere'];
-		}
-
-		foreach( $kommune as $k_id => $data ) {
-			$tot_malgruppe = $data['total']['malgruppe'];
-			$tot_deltakere = $data['total']['deltakere'];
-			$dekning   = (100 / $tot_malgruppe ) * $tot_deltakere;
-			$dekningsgrad[ "$dekning" ] = $k_id;
-		}
-		
-		$top = max( array_keys( $dekningsgrad) );
-		$min = min( array_keys( $dekningsgrad) );
-		
-		unset( $kommune[ $top ]['total'] );
-		unset( $kommune[ $min ]['total'] );
-		unset( $kommune[ $top ][2009] );
-		unset( $kommune[ $min ][2009] );
-		
-		foreach( $TWIG['seasons'] as $ssn ) {
-			$val = $kommune[ $dekningsgrad[$top] ][ $ssn ];
-			$malgruppe[ $ssn ]['I snitt beste kommune'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
-
-			$val = $kommune[ $dekningsgrad[$min] ][ $ssn ];
-			$malgruppe[ $ssn ]['I snitt dårligste kommune'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
-		}
-
-		// FJERN 2009
-		unset( $malgruppe[2009] );
-		// SORTER
-		ksort( $malgruppe );
-		// "LAGRE"
-		$TWIG['statistikk']['fylket']['malgruppe'] = $malgruppe;
-
-} else {
-
-
-
-		// BESTE OG DÅRLIGSTE FYLKE
-		$topbotQry = new SQL("SELECT `f_id`,
-									 `season`,
-									 SUM(`malgruppe`) AS `malgruppe`, 
-									 SUM(`deltakere`) AS `deltakere`
+			
+			
+			// SNITT SMÅ MØNSTRINGER
+			$litenQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
 							   FROM `ukm_statistics_malgruppe`
-							   WHERE `f_id` != 3
-							   AND	`f_id` < 21
-							   GROUP BY `season`, `f_id`
-							   ORDER BY `f_id`, `season`",
-							 array());
-		$topbotRes = $topbotQry->run();
-		
-		while( $r = mysql_fetch_assoc( $topbotRes ) ) {
-			$fylke[ $r['f_id'] ][ $r['season'] ] = array( 'malgruppe' => $r['malgruppe'], 'deltakere' => $r['deltakere'] );
-			$fylke[ $r['f_id'] ][ 'total' ]['malgruppe'] += $r['malgruppe'];
-			$fylke[ $r['f_id'] ][ 'total' ]['deltakere'] += $r['deltakere'];
+							   WHERE `size` = 'liten'
+							   GROUP BY `season`"
+							  );
+			$litenRES = $litenQRY->run();
+			while( $r = mysql_fetch_assoc( $litenRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt små kommuner' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+			
+			
+			// SNITT STORE MØNSTRINGER
+			$storQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
+							   FROM `ukm_statistics_malgruppe`
+							   WHERE `size` = 'stor'
+							   GROUP BY `season`"
+							  );
+			$storRES = $storQRY->run();
+			while( $r = mysql_fetch_assoc( $storRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt store kommuner' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+			
+			
+			// SNITT I FYLKET
+			$fylkeQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
+							   FROM `ukm_statistics_malgruppe`
+							   WHERE `f_id` = '#fylke'
+							   GROUP BY `season`",
+							   array('fylke' => $TWIG['monstring']->fylke->id)
+							  );
+			$fylkeRES = $fylkeQRY->run();
+			while( $r = mysql_fetch_assoc( $fylkeRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt '. $TWIG['monstring']->fylke->name ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+			
+			// SNITT NASJONALT
+			$nasjonalQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
+							   FROM `ukm_statistics_malgruppe`
+							   GROUP BY `season`"
+							  );
+			$nasjonalRES = $nasjonalQRY->run();
+			while( $r = mysql_fetch_assoc( $nasjonalRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt nasjonalt' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+			
+			// FJERN 2009
+			unset( $malgruppe[2009] );
+			// SORTER
+			ksort( $malgruppe );
+			// "LAGRE"
+			$TWIG['statistikk'][$kommune_id]['malgruppe'] = $malgruppe;
 		}
-		
-		foreach( $fylke as $f_id => $data ) {
-			$tot_malgruppe = $data['total']['malgruppe'];
-			$tot_deltakere = $data['total']['deltakere'];
-			$dekning   = (100 / $tot_malgruppe ) * $tot_deltakere;
-			$dekningsgrad[ "$dekning" ] = $f_id;
-		}
-		
-		$top = max( array_keys( $dekningsgrad) );
-		$min = min( array_keys( $dekningsgrad) );
-		
-		unset( $fylke[ $top ]['total'] );
-		unset( $fylke[ $min ]['total'] );
-		unset( $fylke[ $top ][2009] );
-		unset( $fylke[ $min ][2009] );
-
-		foreach( $TWIG['seasons'] as $ssn ) {
-			$val = $fylke[ $dekningsgrad[$top] ][ $ssn ];
-			$malgruppe[ $ssn ]['I snitt beste fylke'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
-
-			$val = $fylke[ $dekningsgrad[$min] ][ $ssn ];
-			$malgruppe[ $ssn ]['I snitt dårligste fylke'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
-		}
-
+	} elseif( $TWIG['stat_type'] == 'fylke' ) {
+			// SNITT I FYLKET
+			$fylkeQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
+							   FROM `ukm_statistics_malgruppe`
+							   WHERE `f_id` = '#fylke'
+							   GROUP BY `season`",
+							   array('fylke' => $TWIG['monstring']->fylke->id)
+							  );
+			$fylkeRES = $fylkeQRY->run();
+			while( $r = mysql_fetch_assoc( $fylkeRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt '. $TWIG['monstring']->fylke->name ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+			
+			// SNITT NASJONALT
+			$nasjonalQRY = new SQL("SELECT `season`,SUM(`malgruppe`) AS `malgruppe`, SUM(`deltakere`) AS `deltakere`
+							   FROM `ukm_statistics_malgruppe`
+							   GROUP BY `season`"
+							  );
+			$nasjonalRES = $nasjonalQRY->run();
+			while( $r = mysql_fetch_assoc( $nasjonalRES ) ) {
+				$malgruppe[ $r['season'] ][ 'Snitt nasjonalt' ] = round( (100/$r['malgruppe'])*$r['deltakere']  ,2);
+			}
+	
+			// BESTE OG DÅRLIGSTE KOMMUNE
+			$topbotQry = new SQL("SELECT `name`,
+										 `ukm_statistics_malgruppe`.`k_id`,
+										 `season`,
+										 SUM(`malgruppe`) AS `malgruppe`, 
+										 SUM(`deltakere`) AS `deltakere`
+								   FROM `ukm_statistics_malgruppe`
+								   LEFT JOIN `smartukm_kommune` AS `k` ON (`k`.`id` = `ukm_statistics_malgruppe`.`k_id`)
+								   WHERE `f_id` = '#fylke'
+								   GROUP BY `season`, `k_id`
+								   ORDER BY `k_id`, `season`",
+								 array('fylke' => $TWIG['monstring']->fylke->id)
+								);
+			$topbotRes = $topbotQry->run();
+			while( $r = mysql_fetch_assoc( $topbotRes ) ) {
+				$data = array( 'malgruppe' => $r['malgruppe'], 'deltakere' => $r['deltakere'], 'dekningsgrad' => round((100/$r['malgruppe'])*$r['deltakere'],2) );
+				$kommune[ $r['k_id'] ][ $r['season'] ] = $data;
+				if($r['season'] != 2009) {
+					$TWIG['dekningsgrad'][ utf8_encode($r['name'])][ $r['season'] ] = $data;
+				}
+				$kommune[ $r['k_id'] ][ 'total' ]['malgruppe'] += $r['malgruppe'];
+				$kommune[ $r['k_id'] ][ 'total' ]['deltakere'] += $r['deltakere'];
+			}
+	
+			foreach( $kommune as $k_id => $data ) {
+				$tot_malgruppe = $data['total']['malgruppe'];
+				$tot_deltakere = $data['total']['deltakere'];
+				$dekning   = (100 / $tot_malgruppe ) * $tot_deltakere;
+				$dekningsgrad[ "$dekning" ] = $k_id;
+			}
+			
+			$top = max( array_keys( $dekningsgrad) );
+			$min = min( array_keys( $dekningsgrad) );
+			
+			unset( $kommune[ $top ]['total'] );
+			unset( $kommune[ $min ]['total'] );
+			unset( $kommune[ $top ][2009] );
+			unset( $kommune[ $min ][2009] );
+			
+			foreach( $TWIG['seasons'] as $ssn ) {
+				$val = $kommune[ $dekningsgrad[$top] ][ $ssn ];
+				$malgruppe[ $ssn ]['I snitt beste kommune'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
+	
+				$val = $kommune[ $dekningsgrad[$min] ][ $ssn ];
+				$malgruppe[ $ssn ]['I snitt dårligste kommune'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
+			}
+	
+			// FJERN 2009
+			unset( $malgruppe[2009] );
+			// SORTER
+			ksort( $malgruppe );
+			// "LAGRE"
+			$TWIG['statistikk']['fylket']['malgruppe'] = $malgruppe;
+	
+	} else {
+	
+	
+	
+			// BESTE OG DÅRLIGSTE FYLKE
+			$topbotQry = new SQL("SELECT `f_id`,
+										 `season`,
+										 SUM(`malgruppe`) AS `malgruppe`, 
+										 SUM(`deltakere`) AS `deltakere`
+								   FROM `ukm_statistics_malgruppe`
+								   WHERE `f_id` != 3
+								   AND	`f_id` < 21
+								   GROUP BY `season`, `f_id`
+								   ORDER BY `f_id`, `season`",
+								 array());
+			$topbotRes = $topbotQry->run();
+			
+			while( $r = mysql_fetch_assoc( $topbotRes ) ) {
+				$fylke[ $r['f_id'] ][ $r['season'] ] = array( 'malgruppe' => $r['malgruppe'], 'deltakere' => $r['deltakere'] );
+				$fylke[ $r['f_id'] ][ 'total' ]['malgruppe'] += $r['malgruppe'];
+				$fylke[ $r['f_id'] ][ 'total' ]['deltakere'] += $r['deltakere'];
+			}
+			
+			foreach( $fylke as $f_id => $data ) {
+				$tot_malgruppe = $data['total']['malgruppe'];
+				$tot_deltakere = $data['total']['deltakere'];
+				$dekning   = (100 / $tot_malgruppe ) * $tot_deltakere;
+				$dekningsgrad[ "$dekning" ] = $f_id;
+			}
+			
+			$top = max( array_keys( $dekningsgrad) );
+			$min = min( array_keys( $dekningsgrad) );
+			
+			unset( $fylke[ $top ]['total'] );
+			unset( $fylke[ $min ]['total'] );
+			unset( $fylke[ $top ][2009] );
+			unset( $fylke[ $min ][2009] );
+	
+			foreach( $TWIG['seasons'] as $ssn ) {
+				$val = $fylke[ $dekningsgrad[$top] ][ $ssn ];
+				$malgruppe[ $ssn ]['I snitt beste fylke'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
+	
+				$val = $fylke[ $dekningsgrad[$min] ][ $ssn ];
+				$malgruppe[ $ssn ]['I snitt dårligste fylke'] = round( (100/$val['malgruppe'])*$val['deltakere'], 2 );
+			}
+	
+	}
 }
