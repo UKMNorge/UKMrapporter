70d8ee51b665f996228783a9dea13fc7528386fd
Marius Mandal <mariusmandal@gmail.com>
2014-01-31 13:58:20 +0100

    Aldersfordeling på plass

--- statistikk/aldersfordeling.controller.php
+++ statistikk/aldersfordeling.controller.php
@@ -1,18 +1,6 @@
 <?php
 if($TWIG['stat_type']=='kommune') {
 	foreach($TWIG['kommuner'] as $kommune_name => $kommune_id){
-		$raw = array();
-		// INIT ARRAY
-		foreach( $TWIG['seasons'] as $ssn ) {
-			$raw[ $ssn ] = array('underti' => 0,
-								 'tiellevetolv' => 0,
-								 'trettenfjorten' => 0,
-								 'femtenseksten' => 0,
-								 'syttenatten' => 0,
-								 'nittentjue' => 0,
-								 'overtjue' => 0,
-								 'total' => 0);
-		}
 		// PERSONER
 		$persQry = new SQL("SELECT `season`,`age`, COUNT(`stat_id`) AS `personer` 
 							FROM `ukm_statistics`
@@ -20,41 +8,74 @@ if($TWIG['stat_type']=='kommune') {
 							GROUP BY `age`, `season`",
 						   array('kommune' => $kommune_id)
 						  );
-		$persRes = $persQry->run();
-		while( $r = mysql_fetch_assoc( $persRes ) ) {
-			$age = (int) $r['age'];
-			$group = group( $age );
-			
-			if( isset( $raw[ $r['season'] ][ $group ] ) )
-				$raw[ $r['season'] ][ $group ] += $r['personer'];
-			else
-				$raw[ $r['season'] ][ $group ] = $r['personer'];
-			
-			$raw[ $r['season'] ]['total'] += $r['personer'];
-		}
-	
-		unset( $raw[2009] );
-		
-		ksort( $raw );
-		
-		$aldersfordeling = $raw;
-		$aldersfordeling_total = $raw;
+		$aldersfordeling = calc_aldersfordeling( $TWIG['seasons'], $persQry );
+		$aldersfordeling_total = $aldersfordeling;
 		
 		foreach( $aldersfordeling as $ssn => $data ) {
 			unset( $data['total'] );		
 			$aldersfordeling[$ssn] = $data;
 		}
-		
 		$TWIG['statistikk'][$kommune_id]['aldersfordeling'] = $aldersfordeling;
 		$TWIG['statistikk'][$kommune_id]['aldersfordeling_total'] = $aldersfordeling_total;
 	}
 } elseif( $TWIG['stat_type'] == 'fylke' ) {
+	foreach($TWIG['kommuner'] as $kommune_name => $kommune_id){
+		// PERSONER
+		$persQry = new SQL("SELECT `season`,`age`, COUNT(`stat_id`) AS `personer` 
+							FROM `ukm_statistics`
+							WHERE `f_id` = '#fylke'
+							GROUP BY `age`, `season`",
+						   array('fylke' => $TWIG['monstring']->fylke->id)
+						  );
+		$aldersfordeling = calc_aldersfordeling( $TWIG['seasons'], $persQry );
+		$aldersfordeling_total = $aldersfordeling;
+		
+		foreach( $aldersfordeling as $ssn => $data ) {
+			unset( $data['total'] );		
+			$aldersfordeling[$ssn] = $data;
+		}
+		$TWIG['statistikk']['fylket']['aldersfordeling'] = $aldersfordeling;
+		$TWIG['statistikk']['fylket']['aldersfordeling_total'] = $aldersfordeling_total;
+	}
 
 } else {
 	$TWIG['error'] = array('header' => 'Statistikk ikke tilgjengelig',
 						   'message'=> 'En systemfeil gjør at det ikke er mulig å beregne statistikk på denne siden. Kontakt UKM Norge'
 						   );
 }
+
+
+
+function calc_aldersfordeling( $seasons, $persQry ) {
+	$raw = array();
+	// INIT ARRAY
+	foreach( $seasons as $ssn ) {
+		$raw[ $ssn ] = array('underti' => 0,
+							 'tiellevetolv' => 0,
+							 'trettenfjorten' => 0,
+							 'femtenseksten' => 0,
+							 'syttenatten' => 0,
+							 'nittentjue' => 0,
+							 'overtjue' => 0,
+							 'total' => 0);
+	}
+	$persRes = $persQry->run();
+	while( $r = mysql_fetch_assoc( $persRes ) ) {
+		$group = group( (int) $r['age'] );
+		
+		if( isset( $raw[ $r['season'] ][ $group ] ) )
+			$raw[ $r['season'] ][ $group ] += $r['personer'];
+		else
+			$raw[ $r['season'] ][ $group ] = $r['personer'];
+		
+		$raw[ $r['season'] ]['total'] += $r['personer'];
+	}
+	unset( $raw[2009] );
+	ksort( $raw );
+	return $raw;
+}
+
+
 
 
 function group( $age ) {
