9087cc9225a6757b84f11a14908c5aaa4dbd8e2f
Marius Mandal <mariusmandal@gmail.com>
2014-06-18 10:47:53 +0200

    også ta med skjulte forestillinger i reCount

--- clean_order_concerts.inc.php
+++ clean_order_concerts.inc.php
@@ -2,7 +2,7 @@
 require_once('UKM/forestilling.class.php');
 $m = new monstring( get_option('pl_id') );
 
-$hendelser = $m->forestillinger();
+$hendelser = $m->forestillinger('c_start', false);
 
 foreach( $hendelser as $hen ) {
 	$h = new forestilling( $hen['c_id'] );
