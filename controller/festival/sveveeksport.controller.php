508011a1224734cf4b78b76eec2bfcba7e8f34f0
Marius Mandal <mariusmandal@gmail.com>
2016-06-18 11:07:08 +0200

    Støtte for å laste ned vCards

--- controller/festival/sveveeksport.controller.php
+++ controller/festival/sveveeksport.controller.php
@@ -1,5 +1,7 @@
 <?php
 require_once('UKM/inc/excel.inc.php');
+require_once('UKM/zip.class.php');
+require_once('UKM/vcard.class.php');
 
 global $objPHPExcel;
 
@@ -10,6 +12,18 @@ while($band_type = mysql_fetch_assoc($band_types))
 
 $m = new monstring(get_option('pl_id'));
 $innslag = $m->innslag_btid();
+
+/** Vcard som zip */
+$zipname = 'UKMfestivalen_deltakere';
+$STORAGE = sys_get_temp_dir() .'/'. $zipname .'/';
+define('ZIP_WRITE_PATH', $STORAGE );
+$zip = new zip($zipname, true);
+$zip->debugMode();
+
+if( !file_exists( $STORAGE ) ) {
+	mkdir( $STORAGE, 0777, true );
+}
+
 
 foreach($innslag as $band_type => $bands) {
 
@@ -35,6 +49,66 @@ foreach($innslag as $band_type => $bands) {
 			excell('A'.$rad,$deltaker['p_firstname'].' '.$deltaker['p_lastname']);
 			excell('B'.$rad,$deltaker['p_phone']);
 		}
+
+		/** 
+		 * Participant vCard (and add to zip)
+		 **/
+		$inn_v2 = new innslag_v2( $band['b_id'] );
+		$kontaktperson_deltar = false;
+		foreach( $inn_v2->getPersoner()->getAllVideresendt( get_option('pl_id') ) as $person ) {
+			if( $inn_v2->getKontaktperson()->getId() == $person->getId() ) {
+				$kontaktperson_deltar = true;
+			}
+			$cardname = 'UKM_participant_'.$person->getId();
+
+			$card = new stdClass();
+			$card->first_name 	= $person->getFornavn();
+			$card->last_name 	= $person->getEtternavn();
+
+			$card->title 		= ($kontaktperson_deltar ? 'KONTAKTPERSON og ' : '') . $person->getInstrument() .' @ '. $inn_v2->getNavn();
+
+			$card->home_tel		= $person->getMobil();
+			$card->fax_tel 		= $person->getMobil() .'600';
+			$card->pager_tel 	= $person->getMobil() .'500';
+
+			$card->email1		= $person->getEpost();
+
+
+			$card->company		= 'UKM '. $inn_v2->getKommune()->getNavn();
+			$card->department	= '('. $inn_v2->getKommune()->getFylke()->getNavn() .')';
+
+			$vcard = new vcard( (array) $card );
+			$vcard->build();
+			$vcard->store( $STORAGE . $cardname, false );
+			// EOVCARD
+			$zip->add( $STORAGE . $cardname.'.vcf', $cardname.'.vcf' );
+		}
+		if( !$kontaktperson_deltar ) {
+			$person = $inn_v2->getKontaktperson();
+			$cardname = 'UKM_participant_'.$person->getId();
+
+			$card = new stdClass();
+			$card->first_name 	= $person->getFornavn();
+			$card->last_name 	= $person->getEtternavn();
+
+			$card->title 		= 'KONTAKTPERSON @ '. $inn_v2->getNavn();
+
+			$card->home_tel		= $person->getMobil();
+			$card->fax_tel 		= $person->getMobil() .'600';
+			$card->pager_tel 	= $person->getMobil() .'500';
+
+			$card->email1		= $person->getEpost();
+
+
+			$card->company		= 'UKM '. $inn_v2->getKommune()->getNavn();
+			$card->department	= '('. $inn_v2->getKommune()->getFylke()->getNavn() .')';
+
+			$vcard = new vcard( (array) $card );
+			$vcard->build();
+			$vcard->store( $STORAGE . $cardname, false );
+			// EOVCARD
+			$zip->add( $STORAGE . $cardname.'.vcf', $cardname.'.vcf' );
+		}
 	}
 	
 	$excelData = new stdClass();
@@ -42,4 +116,6 @@ foreach($innslag as $band_type => $bands) {
 	$excelData->url = exWrite($objPHPExcel, 'UKMF_Sveveksport_'.preg_replace('/[^A-Za-z0-9-.\/]/', '', $excelData->name));
 	
 	$TWIG['excel'][] = $excelData;
+	$TWIG['zip'] = $zip->compress(); //'http://download.ukm.no/zip/'.$zipname; 
+
 }
