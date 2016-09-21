1057c9a33e2078f894a3ff2f213ce01ca96c2029
Marius Mandal <mariusmandal@gmail.com>
2016-09-21 11:11:12 +0200

    ekskluder mønstringer som ikke er registrert

--- controller/network/monstringer_i_fylket.controller.php
+++ controller/network/monstringer_i_fylket.controller.php
@@ -7,6 +7,12 @@ $fylke->setAttr('sesong', $_GET['sesong'] );
 $monstringCollection = new monstringer_v2( $fylke->getAttr('sesong') );
 
 $monstringer = $monstringCollection->utenGjester( $monstringCollection->getAllByFylke( $fylke ) );
+
+foreach( $monstringer as $array_pos => $monstring ) {
+	if( !$monstring->erRegistrert() ) {
+		unset( $monstringer[ $array_pos ] );
+	}
+}
 $fylke->setAttr('monstringer', $monstringer);
 
 $TWIG['fylke'] = $fylke;
