bbb44acac23c625a62637b7d7b8a8b7a6f4dcd58
Marius Mandal <mariusmandal@gmail.com>
2018-09-11 22:49:16 +0200

    zip-fix

--- controller/festival/qr.controller.php
+++ controller/festival/qr.controller.php
@@ -7,22 +7,26 @@ require_once(PLUGIN_DIR .'vendor/autoload.php');
 use PHPQRCode\QRcode;
 use PHPQRCode\Constants;
 
-if( UKM_HOSTNAME == 'ukm.dev' ) {
-	$protocol = 'http:';
-	$path = '/var/www/qr/';
-} else {
-	$protocol = 'https:';
-	$path = '/home/ukmno/public_subdomains/qr/';
-}
-$domain = $protocol . '//qr.'. UKM_HOSTNAME .'/';
+$qr_path = DOWNLOAD_PATH .'qr/';
 
-define('ZIP_WRITE_PATH', $path .'zip/');
+// Hvis mappen ikke eksisterer, opprett
+if( !file_exists( $qr_path ) && is_writable( dirname( $qr_path ) ) ) {
+	@mkdir( $qr_path, 0777 );
+}
+
+// Hvis mappen ikke er skrivbar (eller ikke eksisterer)
+if( !is_writable( $qr_path ) ) {
+	die(
+		'<h2>Beklager, kan ikke generer QR-koder nå. Kontakt UKM Norge</h2>'.
+		'<code>'. $qr_path .'</code>'
+	);
+}
+
 
 $qrkoder = [];
 $zipfiles = [];
 
 $monstring = new monstring_v2( get_option('pl_id') );
-
 if( $monstring->getInnslag()->getAntall() > 0 ) {
 	/**
 	 * ALFABETISK PAKKE
@@ -35,37 +39,38 @@ if( $monstring->getInnslag()->getAntall() > 0 ) {
 		)
 	);
 	$zip = new zip( $zipnavn, true );
-	
 	// Loop alle innslag og lag QR-koder
 	foreach( $monstring->getInnslag()->getAll() as $innslag ) {
 		$qr = new stdClass();
-		$qr->file 	= $monstring->getId() .'-'. $innslag->getId();
-		$qr->url	= $domain . $qr->file;
-		$qr->path 	= $path . $qr->file;
-		$qr->link	= $protocol . $monstring->getLink() . 'pameldte/'. $innslag->getId() .'/';
-	
+		$qr->filename 	= $monstring->getId() .'-'. $innslag->getId() .'.png';
+		$qr->full_path 	= $qr_path . $qr->filename;
+		$qr->link		= 'https://' . $monstring->getLink() . 'pameldte/'. $innslag->getId() .'/';
+
 		$qrkoder[ $innslag->getId() ] = $qr;
 		
 		// PNG-utgave
-		@unlink( $qr->link.'.png');
+		@unlink( $qr->full_path);
 		QRcode::png(
 			$qr->link, 			// STRING text string to encode
-			$qr->path .'.png', 	// STRING output file name, if false outputs to browser with required headers
+			$qr->full_path, 	// STRING output file name, if false outputs to browser with required headers
 			'Q', 				// STRING error correction level L, M, Q or H
 			300, 				// INT pixel size, multiplier for each 'virtual' pixel
 			2,					// INT code margin (silent zone) in 'virtual' pixels
 			false				// BOOL true: code is outputed to browser and saved to file. false: only saved to file
 		);
 		
-		$zip->add(
-			$qr->path .'.png',
-			$innslag->getNavn().'.png'
-		);
+		try {
+			$addRes = $zip->add(
+				$qr->full_path,
+				$innslag->getNavn().'.png'
+			);
+		} catch( Exception $e ) {
+			
+		}
 	}
 	$zipfile = new stdClass();
 	$zipfile->navn = 'Alfabetisk';
-	$zipfile->file = $zip->compress();
-	$zipfile->url = $domain .'zip/'. $zipnavn .'.zip';
+	$zipfile->url = $zip->compress();
 	$TWIG['zip_alfabetisk'] = $zipfile;
 	
 	
@@ -85,17 +90,16 @@ if( $monstring->getInnslag()->getAntall() > 0 ) {
 		foreach( $hendelse->getInnslag()->getAll() as $innslag ) {
 			$qr = $qrkoder[ $innslag->getId() ];
 			
-			$order = $innslag->getAttr('order')+1;
+			$order = $hendelse->getNummer( $innslag );
 			$zip->add(
-				$qr->path .'.png',
+				$qr->full_path,
 				$order .'. '. $innslag->getNavn().'.png'
 			);
 		}
 		
 		$zipfile = new stdClass();
 		$zipfile->navn = $hendelse->getNavn();
-		$zipfile->file = $zip->compress();
-		$zipfile->url = $domain .'zip/'. $zipnavn .'.zip';
+		$zipfile->url = $zip->compress();
 		
 		$zipfiles[] = $zipfile;
 	}
