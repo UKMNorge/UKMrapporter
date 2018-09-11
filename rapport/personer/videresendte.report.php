f2c75994af9906dc69a9cd9e43f91d47735343a8
Marius Mandal <mariusmandal@gmail.com>
2018-09-11 22:48:40 +0200

    phpoffice

--- rapport/personer/videresendte.report.php
+++ rapport/personer/videresendte.report.php
@@ -15,10 +15,6 @@ class valgt_rapport extends rapport {
 	 * @return class object
 	 */
 	public function __construct($rapport, $kategori){
-		if (UKM_HOSTNAME == 'ukm.dev') {
-			define('EXCEL_WRITE_PATH', '/tmp/');
-			define('WORD_WRITE_PATH', '/tmp/');
-		}
 		parent::__construct($rapport, $kategori);
 
 		$this->navn = 'Videresendte fra min mønstring';
