34ca34d39e8b6b7164c6715a49d26aff484b70a5
Asgeir Stavik Hustad <askhus@online.no>
2020-01-07 11:00:21 +0100

    Lagt til rapport for tekniske prøver med notatfelt

---
+++ rapporter/TekniskeProver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UKMNorge\Rapporter;
+
+use UKMNorge\Rapporter\Framework\Gruppe;
+use UKMNorge\Rapporter\Framework\Rapport;
+use UKMNorge\Rapporter\Program;
+
+class TekniskeProver extends Program
+{
+    public $kategori_id = 'program';
+    public $ikon = 'dashicons-editor-ol';
+    public $navn = 'Tekniske Prøver';
+    public $beskrivelse = 'Kjøreplan for tekniske prøver';
+    public $krever_hendelse = true;
+
+}
