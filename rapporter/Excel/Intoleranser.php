28898b55e2433e1fb04f974998931f84566ec1b0
Marius Mandal <mariusmandal@gmail.com>
2020-02-19 12:43:49 +0100

    Rapport for intoleranser

---
+++ rapporter/Excel/Intoleranser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace UKMNorge\Rapporter\Excel;
+
+use UKMNorge\Rapporter\Framework\Config;
+use UKMNorge\Rapporter\Framework\Excel;
+use UKMNorge\File\Excel as ExcelDok;
+use UKMNorge\Allergener\Allergener;
+
+
+class Intoleranser extends Excel {
+
+    public function __construct( String $navn, Array $alle_personer, Config $config ) {
+        $this->config = $config;
+        $this->excel = new ExcelDok( $navn );
+
+        /** HEADERS */
+        $this->excel->setArk('personer', 'Personer');
+        $this->rad();
+        $kolonne = $this->celle('A', 'Navn');
+        $kolonne = $this->celle('B', 'Mobil');
+        $kolonne = $this->celle('C', 'Kommentar');
+        $kolonne = $this->celle('D', 'Oppsummert');
+        foreach( Allergener::getAll() as $allergen ) {
+            $kolonne = $this->celle($kolonne, $allergen->getNavn());
+        }
+
+        $this->rad();
+        $kolonne = $this->celle('A','Sum');
+        $kolonne = $this->celle('B', '');
+        $kolonne = $this->celle('C', '');
+        $kolonne = $this->celle('D', '');
+        foreach( Allergener::getAll() as $allergen ) {
+            $kolonne = $this->celle($kolonne, '=SUM('. $kolonne .'3:'.$kolonne.(sizeof($alle_personer)+2).')');
+        }
+        
+        foreach( $alle_personer as $person ) {
+            $this->rad();
+            $kolonne = $this->celle('A', $person->getNavn());
+            $kolonne = $this->celle('B', $person->getMobil());
+            $kolonne = $this->celle('C', $person->getSensitivt()->getIntoleranse()->getTekst());
+            $kolonne = $this->celle('D', $person->getSensitivt()->getIntoleranse()->getListeHuman(true));
+            foreach( Allergener::getAll() as $allergen ) {
+                $kolonne = $this->celle(
+                    $kolonne,
+                    $person->getSensitivt()->getIntoleranse()->harDenne( $allergen->getId() ) ?
+                        1 : 0
+                );
+            }
+        }
+    }
+}
---
+++ rapporter/Intoleranser.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace UKMNorge\Rapporter;
+
+use UKMNorge\Rapporter\Excel\Intoleranser as ExcelIntoleranser;
+use UKMNorge\Rapporter\Framework\Gruppe;
+use UKMNorge\Rapporter\Framework\Rapport;
+use UKMNorge\Sensitivt\Requester;
+use UKMNorge\Sensitivt\Sensitivt;
+
+class Intoleranser extends Rapport
+{
+    public $kategori_id = 'personer';
+    public $ikon = 'dashicons-carrot';
+    public $navn = 'Allergier og intoleranser';
+    public $beskrivelse = 'OBS: Gjelder kun videresendte innslag! Lister ut allergier og intoleranser til deltakerne.';
+    public $krever_hendelse = false;
+
+    public function getRenderData()
+    {
+        Sensitivt::setRequester(
+            new Requester(
+                'wordpress', 
+                wp_get_current_user()->ID,
+                get_option('pl_id')
+            )
+        );
+
+        $gruppe = new Gruppe('container', 'Deltakere med intoleranser / allergier');
+        $gruppe->setVisOverskrift(true);
+
+        foreach( $this->getArrangement()->getInnslag()->getAll() as $innslag ) {
+            foreach( $innslag->getPersoner()->getAll() as $person ) {
+                if( !$person->getSensitivt()->getIntoleranse()->har() ) {
+                    continue;
+                }
+                $gruppe->addPerson($person);
+            }
+        }
+        return $gruppe;
+    }
+    
+    /**
+     * Lag og returner excel-filens URL
+     * 
+     * @return String url
+     */
+    public function getExcelFile()
+    {
+        $excel = new ExcelIntoleranser(
+            $this->getNavn() . ' oppdatert ' . date('d-m-Y') . ' kl '. date('Hi') . ' - ' . $this->getArrangement()->getNavn(),
+            $this->getRenderDataPersoner(),
+            $this->getConfig()
+        );
+        return $excel->writeToFile();
+    }
+}
