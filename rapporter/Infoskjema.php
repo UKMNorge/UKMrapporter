75f163039176b8761715b032589a65c9db621ed0
Marius Mandal <mariusmandal@gmail.com>
2020-02-21 15:06:43 +0100

    Rapport for spørreskjema

---
+++ rapporter/Infoskjema.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace UKMNorge\Rapporter;
+
+use UKMNorge\Innslag\Samling;
+use UKMNorge\Rapporter\Framework\Gruppe;
+use UKMNorge\Rapporter\Framework\Rapport;
+
+class Infoskjema extends Rapport
+{
+    public $kategori_id = 'videresending';
+    public $ikon = 'dashicons-forms';
+    public $navn = 'Spørreskjema';
+    public $beskrivelse = 'Svar på spørreskjema til de som videresender';
+    public $har_excel = false;
+    public $har_sms = false;
+    public $har_epost = false;
+    
+
+    public function getRenderData() {
+        $grupper = new Gruppe('container', 'Nøkkeltall');
+
+    }
+
+    /**
+     * Hent hvilken template som skal benyttes
+     *
+     * @return String $template_id
+     */
+    public function getTemplate()
+    {
+        return 'Infoskjema/rapport.html.twig';
+    }
+}
