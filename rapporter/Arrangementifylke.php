c1ce73488b985e5aaea1eafdf3d60565b2d8fee7
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2021-04-22 17:10:29 +0200

    Sorterer arrangement og ferdig arrangement dato

--- rapporter/Arrangementifylke.php
+++ rapporter/Arrangementifylke.php
@@ -63,7 +63,6 @@ class Arrangementifylke extends Rapport
                 ];
             }
         }
-
         
         UKMrapporter::addViewData('kommuner', $kommuner);
         return 'Arrangementifylke/rapport.html.twig';
@@ -76,6 +75,47 @@ class Arrangementifylke extends Rapport
             $arrangementer[] = $arrangement;
         }
 
+        // Sorter arrangemeter når sortering er 'start_dato' 
+        if($this->getConfig()->get('sortering') == 'start_dato') {
+            $arrangementer = $this->array_sort($arrangementer, 'start');
+        }
+
         return $arrangementer;
+    }
+
+    // Hentet fra https://www.php.net/manual/en/function.sort.php
+    private function array_sort($array, $on, $order=SORT_ASC)
+    {
+        $new_array = array();
+        $sortable_array = array();
+    
+        if (count($array) > 0) {
+            foreach ($array as $k => $v) {
+                if (is_array($v)) {
+                    foreach ($v as $k2 => $v2) {
+                        if ($k2 == $on) {
+                            $sortable_array[$k] = $v2;
+                        }
+                    }
+                } else {
+                    $sortable_array[$k] = $v;
+                }
+            }
+
+            switch ($order) {
+                case SORT_ASC:
+                    asort($sortable_array);
+                break;
+                case SORT_DESC:
+                    arsort($sortable_array);
+                break;
+            }
+    
+            foreach ($sortable_array as $k => $v) {
+                $new_array[$k] = $array[$k];
+            }
+        }
+    
+        return $new_array;
     }
 }
