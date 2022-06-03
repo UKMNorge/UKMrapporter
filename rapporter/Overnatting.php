e7f089cbb026508673f58b34a0fd81a42744d39a
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-06-03 09:37:25 +0200

    Overnattinger henter fylker bestillinger
    Overnattinger kalles nå Hotellbestillinger og henter fylker bestillinger i tillegg. Det inkluderes kommentarer til slutten av rapporten for fylkene som ble vagt.

--- rapporter/Overnatting.php
+++ rapporter/Overnatting.php
@@ -7,6 +7,10 @@ use UKMNorge\Arrangement\Skjema\Skjema;
 use UKMNorge\Rapporter\Framework\Rapport;
 use UKMNorge\Arrangement\UKMFestival;
 use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\Geografi\Fylker;
+use UKMNorge\Arrangement\Videresending\Ledere\Ledere;
+
+
 use UKMrapporter;
 
 
@@ -56,20 +60,20 @@ class Overnatting extends Rapport
     {
         $arrangement = $this->getArrangement();
 
-       
+        $fylker = Fylker::getAll();
         try {
             if($arrangement->getEierType() == 'land') {
                 $arrangement = new UKMFestival($arrangement->getId());
-                return ['skjema' => $this->getArrangement()->getSkjema(), 'overnattingGrupper' => $arrangement->getOvernattingGrupper()];
+                return ['alleFylker' => $fylker, 'skjema' => $this->getArrangement()->getSkjema(), 'overnattingGrupper' => $arrangement->getOvernattingGrupper()];
             }
             
-            return ['skjema' => $this->getArrangement()->getSkjema()];
+            return ['alleFylker' => $fylker, 'skjema' => $this->getArrangement()->getSkjema()];
         } catch (Exception $e) {
             if ($e->getCode() != 151002) {
                 throw $e;
             }
         }
-        return [];
+        return ['alleFylker' => $fylker];
     }
 
     /**
@@ -79,14 +83,91 @@ class Overnatting extends Rapport
      */
     public function getTemplate()
     {
-        $arrangement = $this->getArrangement();
+        $til = $this->getArrangement();
 
-        if($arrangement->getEierType() == 'land') {
-            $arrangement = new UKMFestival($arrangement->getId());
-            UKMrapporter::addViewData('overnattingGrupper', $arrangement->getOvernattingGrupper());
+        if($til->getEierType() == 'land') {
+            $til = new UKMFestival($til->getId());
+            UKMrapporter::addViewData('overnattingGrupper', $til->getOvernattingGrupper());
         }
+
+
+        // Fylker
+        $selectedFylker = [];
+        // hvis brukeren velger ingen av alternativene så skal alle fylker vises
+        if(count($this->getConfig()->getAll()) < 1) {
+            $selectedFylker = Fylker::getAll();
+        }
+
+        // Valgte fylker
+        foreach($this->getConfig()->getAll() as $selectedItem) {
+            if($selectedItem->getId() == 'vis_fylke_alle') {
+                $selectedFylker = [];
+                $selectedFylker = Fylker::getAll();
+                break;
+            }
+            try{
+                if(strpos($selectedItem->getId(), 'vis_fylke') !== false) {
+                    // struktur av strengen er: 'vis_fylke_00'
+                    $fylkeId = (int) explode("_", $selectedItem->getId(), 3)[2];
+                    $selectedFylker[] = Fylker::getById($fylkeId);
+                }
+            }catch(Exception $e) {
+                throw new Exception(
+                    'Beklager, fylke kunne ikke leses ' . $selectedItem->getId(),
+                    100001004
+                );
+            }
+        }
+
+        $arrangementer = [];
+        $netter = [];
+        $kommentarer = [];
+
+
+        // Alle arrangementer som ble videresend til $til
+        foreach($til->getVideresending()->getAvsendere() as $avsender) {
+            $fra = $avsender->getArrangement();
+            $kommentarer[$fra->getFylke()->getId()][$fra->getId()] = $fra->getMetaValue('kommentar_overnatting_til_' . $til->getId());
+            
+            // For alle fylker som ble valgt
+            foreach($selectedFylker as $fylke) {
+                $fylker[$fylke->getId()] = $fylke;                
+
+                // Hvis $fra (arrangement som ble videresendt) er fra fylke som er valgt
+                if($fylke->getId() == $fra->getFylke()->getId()) {
+                    $arrangementer[$fra->getId()] = $fra;
+                    
+                    $ledere = new Ledere($fra->getId(), $til->getId());
+
+                    foreach($ledere->getAll() as $leder) {
+                    
+                        foreach($leder->getNetter()->getAll() as $natt) {
+                            // Bare ledere som overnatter i landsbyen skal bli med i rapporten
+                            $netter[$natt->getId()]['fylker'][$fylke->getId()][$fra->getId()][] = $leder;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Get alle fritekst kommentarer fra fylkene angående overnatting bestiling
         
-        UKMrapporter::addViewData('gruppe', (int) $this->getConfig()->get('vis_gruppe')->getValue());
-        return 'Overnatting/rapport.html.twig';
+        UKMrapporter::addViewData('netter', $netter);
+        UKMrapporter::addViewData('fylker', $fylker);
+        UKMrapporter::addViewData('arrangementer', $arrangementer);
+        UKMrapporter::addViewData('kommentarer', $kommentarer);
+
+        // Hvis brukeren har sagt ja til hotellbestilling lokalt
+        if($this->getConfig()->get('vis_overnatting_videresending')->getValue() == 'no') {
+            if($this->getConfig()->get('vis_gruppe')) {
+                UKMrapporter::addViewData('gruppe', (int) $this->getConfig()->get('vis_gruppe')->getValue());
+            }
+            return 'Overnatting/rapport.html.twig';
+
+        }
+        else {
+            return 'Overnatting/rapport_fylke_bestillinger.html.twig';
+        }
+
     }
 }
