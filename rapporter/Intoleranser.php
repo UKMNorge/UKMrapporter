28ab964bb80bbdafd93ff7eee02742271b9eaa83
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-04-11 23:44:18 +0200

    Rapporter - intoleranser for ledere feil fikset

--- rapporter/Intoleranser.php
+++ rapporter/Intoleranser.php
@@ -59,28 +59,30 @@ class Intoleranser extends Rapport
                         $ledere = new Ledere($fra->getId(), $til->getId());
                         $ledereMed = [];
                         foreach($ledere->getAll() as $leder) {
-                            // turist, ledsager og sykerom blir ikke med i rapporten
-                            if(!in_array($leder->getType(), ['turist', 'ledsager', 'hoved', 'reise'])) {
-
-                                $leder->getSensitivt()->getIntoleranse();
-
-                                $fylke_gruppe_id = $fylke->getNavn() . '-' . $fylke->getId();
-    
-                                if (!$gruppe->harGruppe($fylke_gruppe_id)) {
-                                    $gruppe->addGruppe(
-                                        new Gruppe(
-                                            $fylke_gruppe_id,
-                                            $fylke->getNavn()
-                                        )
-                                    );
-                                }
-                                $leder->setNavn($leder->getNavn() . ' (Leder)');
-                                $ledereMed[] = $leder;
+                            if( !$leder->getSensitivt()->getIntoleranse()->har() ) {
+                                continue;
                             }
+                            
+                            $leder->getSensitivt()->getIntoleranse();
+
+                            $fylke_gruppe_id = $fylke->getNavn() . '-' . $fylke->getId();
+
+                            if (!$gruppe->harGruppe($fylke_gruppe_id)) {
+                                $gruppe->addGruppe(
+                                    new Gruppe(
+                                        $fylke_gruppe_id,
+                                        $fylke->getNavn()
+                                    )
+                                );
+                            }
+                            $leder->setNavn($leder->getNavn() . ' ('. $leder->getTypeNavn() .')');
+                            $ledereMed[] = $leder;
                         }
                         // Merger personer og ledere. Dette kan feile og her brukes kun som representasjon på brukergrensesnitt. Person og Leder er veldig forskjellige men metodene som kalles på GUI i dette tilfelle finnes i begge klassene
-                        $personerLedere = array_merge($gruppe->getGruppe($fylke_gruppe_id)->getPersoner(), $ledereMed);
-                        $gruppe->getGruppe($fylke_gruppe_id)->setPersoner($personerLedere);
+                        if($gruppe->getGruppe($fylke_gruppe_id)) {
+                            $personerLedere = array_merge($gruppe->getGruppe($fylke_gruppe_id)->getPersoner(), $ledereMed);
+                            $gruppe->getGruppe($fylke_gruppe_id)->setPersoner($personerLedere);
+                        }
                     }
                 }
             }
