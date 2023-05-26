2632d15f4566a59eb41017702fbc7ecf9a5b2d3d
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-05-26 16:38:18 +0200

    Fikser feil på Timeplan rapport

--- rapporter/Timeplan.php
+++ rapporter/Timeplan.php
@@ -41,23 +41,14 @@ class Timeplan extends Rapport
         }
 
         $arrangement = $this->getArrangement();
-        $hendelser = [];
         $dager = [];
         foreach($arrangement->getProgram()->getAbsoluteAll() as $hendelse) {
             $hendelse->getInnslag()->getAll();
-            $hendelse->getTid();
-            $hendelser[] = $hendelse;
-            foreach($hendelse->getInnslag()->getAll() as $innslag) {
-                // Hvis ingen dag selektert vis alle dager eller hvis alle dager selektert vis alle eller sjekk dag og legg til hvis dag er selektert
-                if((count($selectedDager) == 0) || ($selectedDager[0] == 'vis_dager_alle') || (in_array("vis_" . $hendelse->getOppmoteTid($innslag)->format('d_m_Y'), $selectedDager))) {
-                    $dager[$hendelse->getOppmoteTid($innslag)->format('d.m.Y')]['hendelse'][$hendelse->getId()] = $hendelse;
-                    $dager[$hendelse->getOppmoteTid($innslag)->format('d.m.Y')]['innslag'][] = $innslag;
-                }
+            if((count($selectedDager) == 0) || ($selectedDager[0] == 'vis_dager_alle') || (in_array("vis_" . $hendelse->getStart()->format('d_m_Y'), $selectedDager))) {
+                $dager[$hendelse->getStart()->format('d.m.Y')][$hendelse->getId()] = $hendelse;
             }
         }
 
-        UKMrapporter::addViewData('sorteringMetode', -1);
-        UKMrapporter::addViewData('hendelser', $hendelser);
         UKMrapporter::addViewData('dager', $dager);
 
         return 'Timeplan/rapport.html.twig';
