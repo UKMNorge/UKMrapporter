319e12beac49c30e2036d42be057721b52e0efa3
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-06-19 03:16:08 +0200

    Grupperer etter arrangementer på timeplan

--- rapporter/Timeplan.php
+++ rapporter/Timeplan.php
@@ -32,20 +32,37 @@ class Timeplan extends Rapport
         foreach($this->getConfig()->getAll() as $selectedDag) {
             $selectedFylker[] = $selectedDag->getId();
         }
-
+        $alleArrangementer = [];
+        $alleHendelser = [];
         $arrangement = $this->getArrangement();
         $fylker = [];
+
         foreach($arrangement->getProgram()->getAbsoluteAll() as $hendelse) {
             $hendelse->getInnslag()->getAll();
             foreach($hendelse->getInnslag()->getAll() as $innslag) {
                 if((count($selectedFylker) == 0) || ($selectedFylker[0] == 'vis_fylke_alle') || (in_array("vis_fylke_" . $innslag->getFylke()->getId(), $selectedFylker))) {
-                    $fylker[$innslag->getFylke()->getNavn()][$hendelse->getStart()->format('d.m.Y')][$hendelse->getId()] = $hendelse;
+
+                    $fraArrangKey = 0; 
+
+                    // OBS: her brukes fylke id fordi 2 fylker hadde flere arrangementer og det var ønskelig å sortere etter arrangementer. Dette er kun en quick fix og ikke en løsning!
+                    if($innslag->getFylke()->getId() == 30 || $innslag->getFylke()->getId() == 54) {
+                        $fraArrangement = $arrangement->getVideresendingArrangement($innslag->getId());
+                        if($fraArrangement) {
+                            $fraArrangKey = $fraArrangement->getId();
+                            $alleArrangementer[$fraArrangement->getId()] = $fraArrangement;
+                        }
+                    }
+                    
+                    $alleHendelser[$hendelse->getId()] = $hendelse;
+                    $fylker[$fraArrangKey][$innslag->getFylke()->getNavn()][$hendelse->getStart()->format('d.m.Y')][$hendelse->getId()][] = $innslag;
                 }
             }
             
         }
 
         UKMrapporter::addViewData('fylker', $fylker);
+        UKMrapporter::addViewData('alleArrangementer', $alleArrangementer);
+        UKMrapporter::addViewData('alleHendelser', $alleHendelser);
 
         return 'Timeplan/rapport.html.twig';
     }
