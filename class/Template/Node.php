2c2f9c9b059c0dcfe4cabdfbe981ea9a87d31d09
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-07-20 15:22:34 +0200

    Ny klasse for å organisere noder på server

---
+++ class/Template/Node.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace UKMNorge\Rapporter\Template;
+
+class Node {
+
+    var $name;
+    var $children = [];
+    var $obj;
+    
+    public function __construct($name, $obj) {
+        $this->name = $name;
+        $this->obj = $obj;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function addChildren($children) {
+        $this->children = $children;
+    }
+
+    public function addChild($id, $child) {
+        $this->children[$id] = $child;
+    }
+
+    public function getChildren() {
+        return $this->children;
+    }
+
+    public function getObj() {
+        return $this->obj;
+    }
+}
