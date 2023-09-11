c4100f78b6730266b1f691fad359f1a96558f59e
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-09-11 15:37:04 +0200

    Sortering på Table
    Sortering på table inkludering inkludert markering av knapper
    Style på fjern knapp på keys

--- client/src/components/table/Table.vue
+++ client/src/components/table/Table.vue
@@ -35,30 +35,37 @@
                                 </button>
                             </div>
                         </th>
-                        <template v-for="keyObj in keys">
-                            <template v-for="key in keyObj.value">
+                        <!-- <template v-for="(keyObj, index) in keys" :key="index"> -->
+                            <template v-for="(key, pos) in getActiveKeys()" :key="pos">
                                 <th v-if="key.active" scope="col">
                                     <div class="inner-div">
-                                        <button @click="sortBy(key)" class="sort-button">
+                                        <button :class="{'active-sort' : sortActivated && sortPosition == pos}" @click="setSort(pos)" class="sort-button">
                                             <span class="title">{{ key.navn }}</span>
                                             <div class="indicators">
                                                 <div>
-                                                    <svg :class="{'not-active' : currentSort == key.navn && ascSort}" class="sort-indicator" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 6 19 1" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path></svg>
+                                                    <svg :class="{'not-active' : sortActivated && ascSort == false}" class="sort-indicator" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 6 19 1" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="m6.293 13.293 1.414 1.414L12 10.414l4.293 4.293 1.414-1.414L12 7.586z"></path></svg>
                                                 </div>
                                                 <div>
-                                                    <svg :class="{'not-active' : currentSort == key.navn && !ascSort}" class="sort-indicator" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 7 19 18" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>
+                                                    <svg :class="{'not-active' : sortActivated && ascSort == true}" class="sort-indicator" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 7 19 18" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"></path></svg>
                                                 </div>
                                             </div>
                                         </button>
-                                        <button @click="removeProperty(key)" class="remove-row ukm-botton-style not-correct-button">
+                                        <!-- <button @click="removeProperty(key)" class="remove-row ukm-botton-style not-correct-button">
                                             <svg class="remove-icon close-selector" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path class="close-selector" d="M11.5 4.24264L10.0858 2.82843L7.25736 5.65685L4.42893 2.82843L3.01472 4.24264L5.84315 7.07107L3.01472 9.89949L4.42893 11.3137L7.25736 8.48528L10.0858 11.3137L11.5 9.89949L8.67157 7.07107L11.5 4.24264Z" fill="#9B9B9B"/>
                                             </svg>
+                                        </button> -->
+                                        <button @click="removeProperty(key)" class="close-selector close-btn remove-prop as-btn-hover-default">
+                                            <div class="icon close-selector">
+                                                <svg class="remove-icon close-selector" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path class="close-selector" d="M11.5 4.24264L10.0858 2.82843L7.25736 5.65685L4.42893 2.82843L3.01472 4.24264L5.84315 7.07107L3.01472 9.89949L4.42893 11.3137L7.25736 8.48528L10.0858 11.3137L11.5 9.89949L8.67157 7.07107L11.5 4.24264Z" fill="#9B9B9B"/>
+                                                </svg>
+                                            </div>
                                         </button>
                                     </div>
                                 </th>
                             </template>
-                        </template>
+                        <!-- </template> -->
                     </tr>
                 </thead>
                 <tbody>
@@ -73,7 +80,7 @@
                 <h5>Antall{{ uniqueCount ? ' unike' : '' }}: {{ countLeafNodesItems() }}</h5>
             </div>
         </div>
-        
+               
     </div>
 </template>
 
@@ -101,6 +108,10 @@
     var values : any = ref([]);
     var uniqueCount : any = ref(false);
 
+    var ascSort : any = ref(true);
+    var sortPosition : any = ref(-1);
+    var sortActivated : any = ref(false); 
+
     var visAntall = ref(props.visAntall);
     var visTelling = ref(props.visTelling);
 
@@ -120,18 +131,52 @@
     }
 
     var currentSort : string = '';
-    var ascSort : boolean = true;
 
-    function sortBy(key : {navn : string, method : string , active: Boolean}) {
-        currentSort = key.navn;
+    function getActiveKeys() : NodeProperty[] {
+        var ret : NodeProperty[] = [];
 
-        values.sort((a : any, b : any) => {return a[key.method]() > b[key.method]() ? 1 : (a[key.method]() < b[key.method]() ? -1 : 0)})
-
-        if(ascSort) {
-            values.reverse();
+        for(var np of props.keys) {
+            for(var value of np.value) {
+                if(value.active) {
+                    ret.push(value);
+                }
+            }
         }
 
-        ascSort = !ascSort
+        return ret;
+    }
+
+    function setSort(sortPos : Number) {
+        if(sortPosition.value != sortPos) {
+            sortActivated.value = true;
+            ascSort.value = true;
+        }
+        else if(sortActivated.value == true && !ascSort.value) {
+            sortActivated.value = false;
+            ascSort.value = true;
+        }
+        else if(sortActivated.value == false) {
+            sortActivated.value = true;
+        }
+        else {
+            sortActivated.value = true;
+            ascSort.value = false;
+        }
+
+
+        sortPosition.value = sortPos;
+
+    }
+
+    function sortBy(items : any[]) {
+        var pos = sortPosition.value;
+        var sortedItems = items.sort((a : any, b : any) => {return a[pos] > b[pos] ? 1 : (a[pos] < b[pos] ? -1 : 0)});
+
+        return ascSort.value == true ? sortedItems : sortedItems.reverse();
+    }
+
+    function getSortPosition() : Number {
+        return 0;
     }
 
     // Get all items from the classes sendt as array on values
@@ -143,7 +188,7 @@
             }
         }
 
-        return items;
+        return sortActivated.value == true ? sortBy(items) : items;
     }
 
     /* 
@@ -235,12 +280,15 @@
     }
 </script>
 
-<style>
+<style scoped>
     .ukm-vue-table-row {
         margin: 0;
     }
     .ukm-vue-table-row thead tr th .inner-div {
         display: flex;
+    }
+    .ukm-vue-table-row thead tr th .inner-div .indicators {
+        margin-top: 2px;
     }
     .ukm-vue-table-row thead tr th .inner-div .indicators div {
         display: flex;
@@ -283,6 +331,9 @@
         display: flex;
         padding: 0;
     }
+    .ukm-vue-table-row thead tr th .sort-button.active-sort .indicators div  {
+        visibility: visible;
+    }
     .ukm-vue-table-row thead tr th .sort-button .title {
         margin: auto;
         margin-right: 5px;
@@ -292,5 +343,34 @@
     }
     .table > thead > tr > th {
         
+    }
+    button.close-btn.remove-prop {
+        visibility: hidden;
+        opacity: 0;
+        border: none;
+        border-radius: 50%;
+        height: 35px;
+        width: 35px;
+        display: flex;
+        background: transparent;
+        transition: opacity .1s;
+        margin-left: 5px;
+    }
+    .ukm-vue-table-row thead tr th .inner-div:hover button.close-btn.remove-prop {
+        visibility: visible;
+        opacity: 1;
+        transition: opacity .1s;
+    }
+    button.close-btn.remove-prop:hover {
+        background: var(--color-primary-grey-light) !important;        
+    }
+    button.close-btn.remove-prop:hover .icon svg path {
+        fill: var(--color-primary-grey-dark) !important;
+    }
+    button.close-btn.remove-prop .icon {
+        margin: auto
+    }
+    button.close-btn.remove-prop .icon svg {
+        display: flex;
     }
 </style>
