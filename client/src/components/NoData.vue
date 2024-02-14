7a4c38ede14463bd5c2b4416d131d0dc0d13f7ac
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-02-14 16:11:46 +0100

    No data image

--- client/src/components/NoData.vue
+++ client/src/components/NoData.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="as-container container">
         <div class="nop as-margin-top-space-5 as-card-1 as-padding-space-3">
-            <h5>Det er ikke data for denne rapporten!</h5>                
+            <h5 class="text-info-no-data">Det finnes ingen tilgjengelige data for denne rapporten.</h5>                
+
+            <div class="img-no-data">
+                <img :src="imgNoData" alt="Ingen data" />
+            </div>
+            
         </div>
     </div>
 </template>
+<script setup lang="ts">
+    import { ref } from 'vue';
+    let imgNoData = "https://assets."+ UKM_HOSTNAME +"/img/rapporter/no-data-for-rapport.png";
+</script>
+
+<style scoped>
+    .text-info-no-data {
+        text-align: center;
+        margin-bottom: 50px;
+    }
+    .img-no-data {
+        text-align: center;
+    }
+    .img-no-data img {
+        width: 100%;
+        max-width: 100px;
+    }
+</style>
