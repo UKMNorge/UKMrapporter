432f1500a9fed303919aff3bfc41e239f0883684
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-02-20 17:59:17 +0100

    Fikser space bare

--- client/src/components/SendSMS.vue
+++ client/src/components/SendSMS.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <button class="as-btn-simple as-btn-hover-default btn-with-icon" @click="openSelector()">
+            <button class="as-btn-simple as-btn-hover-default btn-with-icon as-margin-left-space-3" @click="openSelector()">
                 <svg class="as-margin-right-space-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 5.589 2 10c0 2.908 1.897 5.516 5 6.934V22l5.34-4.004C17.697 17.852 22 14.32 22 10c0-4.411-4.486-8-10-8zm-2.5 9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"></path></svg>
                 <span>Send SMS</span>
             </button>
