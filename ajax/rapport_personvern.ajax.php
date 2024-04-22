d9828c2e21f5d19e1ecb4bdb1c46116fabd201d9
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-04-22 13:14:07 +0200

    Fikser issue på personvern rapport
    Henter innslag direkte fra arrangement

--- ajax/rapport_personvern.ajax.php
+++ ajax/rapport_personvern.ajax.php
@@ -16,57 +16,54 @@ $innslags = [];
 $retObj = [];
 
 
-foreach( $arrangement->getProgram()->getAbsoluteAll() as $hendelse ) {
-    foreach($hendelse->getInnslag()->getAll() as $innslag) {
-        
-        $innslagObj = [
-            'id' => $innslag->getId(),
-            'navn' => $innslag->getNavn(),
-            'type' => $innslag->getType()->getKey(),
-            'sesong' => $innslag->getSesong(),
-            'arrangement' => $innslag->getHome()->getNavn(),
-            'alle_personer' => [],
-            'alle_titler' => [],
+foreach( $arrangement->getInnslag()->getAll() as $innslag ) {
+    $innslagObj = [
+        'id' => $innslag->getId(),
+        'navn' => $innslag->getNavn(),
+        'type' => $innslag->getType()->getKey(),
+        'sesong' => $innslag->getSesong(),
+        'arrangement' => $innslag->getHome()->getNavn(),
+        'alle_personer' => [],
+        'alle_titler' => [],
+    ];
+    
+    foreach($innslag->getSamtykke()->getAll() as $person) {
+        $innslagPerson = $person->getPerson();
+        $personObj = [
+            'id' => $person->getId(),
+            'fornavn' => $innslagPerson->getFornavn(),
+            'etternavn' => $innslagPerson->getEtternavn(),
+            'mobil' => $innslagPerson->getMobil(),
+            'epost' => $innslagPerson->getEpost(),
+            'status' => $person->getStatus()->getId() != "ikke_godkjent" ? "Godkjent samtykke" : "Ikke godkjent samtykke",
+            'foresatt' => null,
+            'foresatt_mobil' => null,
+            'kategori' => $person->getKategori()->getId(),
+            'godkjent' => $person->getStatus()->getId() != 'ikke_godkjent' ? true : false,
         ];
-        
-        foreach($innslag->getSamtykke()->getAll() as $person) {
-            $innslagPerson = $person->getPerson();
-            $personObj = [
-                'id' => $person->getId(),
-                'fornavn' => $innslagPerson->getFornavn(),
-                'etternavn' => $innslagPerson->getEtternavn(),
-                'mobil' => $innslagPerson->getMobil(),
-                'epost' => $innslagPerson->getEpost(),
-                'status' => $person->getStatus()->getId() != "ikke_godkjent" ? "Godkjent samtykke" : "Ikke godkjent samtykke",
-                'foresatt' => null,
-                'foresatt_mobil' => null,
-                'kategori' => $person->getKategori()->getId(),
-                'godkjent' => $person->getStatus()->getId() != 'ikke_godkjent' ? true : false,
-            ];
-            // Personen er under 15 og derfor er det foresatt som skal utføre godkjenning
-            if($person->getKategori()->getId() == 'u15') {
-                $personObj['foresatt'] = $person->getForesatt()->getNavn();
-                $personObj['foresatt_mobil'] = $person->getForesatt()->getMobil();
-                $personObj['foresatt_status'] = $person->getForesatt()->getStatus()->getId() != "ikke_godkjent" ? "Godkjent samtykke" : "Ikke godkjent samtykke";
-            }
-            $innslagObj['alle_personer'][] = $personObj;
+        // Personen er under 15 og derfor er det foresatt som skal utføre godkjenning
+        if($person->getKategori()->getId() == 'u15') {
+            $personObj['foresatt'] = $person->getForesatt()->getNavn();
+            $personObj['foresatt_mobil'] = $person->getForesatt()->getMobil();
+            $personObj['foresatt_status'] = $person->getForesatt()->getStatus()->getId() != "ikke_godkjent" ? "Godkjent samtykke" : "Ikke godkjent samtykke";
         }
-        
-        try {
-            foreach($innslag->getTitler()->getAll() as $tittel) {
-                $innslagObj['alle_titler'][] = $tittel;
-            }
-        } catch(Exception $e) {
-            // Tittel kan ikke opprettes. Mest sannsynlig finnes det ikke klasse per noen innslag typer som kan ikke ha titler
-        }
-
-        // Adding hendelse
-        if(!key_exists($innslag->getId(), $innslags)) {
-            $innslags[$innslag->getId()] = new Node('Innslag', $innslagObj);
-            $root->addChild($innslag->getId(), $innslags[$innslag->getId()]);
-        }
-        // $hendelser[$hendelse->getId()]->addChild($innslag->getId(), $innslags[$innslag->getId()]);
+        $innslagObj['alle_personer'][] = $personObj;
     }
+    
+    try {
+        foreach($innslag->getTitler()->getAll() as $tittel) {
+            $innslagObj['alle_titler'][] = $tittel;
+        }
+    } catch(Exception $e) {
+        // Tittel kan ikke opprettes. Mest sannsynlig finnes det ikke klasse per noen innslag typer som kan ikke ha titler
+    }
+
+    // Adding hendelse
+    if(!key_exists($innslag->getId(), $innslags)) {
+        $innslags[$innslag->getId()] = new Node('Innslag', $innslagObj);
+        $root->addChild($innslag->getId(), $innslags[$innslag->getId()]);
+    }
+    // $hendelser[$hendelse->getId()]->addChild($innslag->getId(), $innslags[$innslag->getId()]);
 }
 
 $arrRes = [
